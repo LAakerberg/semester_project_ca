98898f1abdeaed56489cce9eb12ad2b69da07e8b
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-11-25 22:18:44 +0100

    Moving all file's from zero_bud folder

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "./pages/**/*.html",
+    "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
