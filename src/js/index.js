84d9c42da060abe9c10cba0b9a80c88260f3f685
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-01 22:53:27 +0100

    Added index.js for open of modal to login

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log('Intentional Syntax Error');
+console.log('Intentional Syntax Error...');
+
+const modal = document.querySelector('#loginModal');
+const btn = document.querySelector('#loginBtn');
+const btnClose = document.querySelector('#close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+btnClose.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
