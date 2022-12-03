3cde6c99887ee42a47b61e69a51897a640db080c
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-03 20:03:07 +0100

    Added JS functionallyt for open/close modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,19 @@
 console.log('Intentional Syntax Error...');
 
+const signUpModal = document.querySelector('#signupModal');
+const signUpBtn = document.querySelector('#registerBtn');
+const signUpBtnClose = document.querySelector('#signup-close');
 const modal = document.querySelector('#loginModal');
 const btn = document.querySelector('#loginBtn');
 const btnClose = document.querySelector('#close');
+
+signUpBtn.onclick = function () {
+  signUpModal.style.display = 'block';
+};
+
+signUpBtnClose.onclick = function () {
+  signUpModal.style.display = 'none';
+};
 
 btn.onclick = function () {
   modal.style.display = 'block';
@@ -17,3 +28,5 @@ window.onclick = function (event) {
     modal.style.display = 'none';
   }
 };
+
+console.log(' Internal ERROR ');
