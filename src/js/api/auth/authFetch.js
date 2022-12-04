23743c913038ab47a4d5e3de7a388c8cafaea6f9
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-04 23:22:37 +0100

    Added authFetch, not in use per now

---
+++ src/js/api/auth/authFetch.js
@@ -0,0 +1,16 @@
+/* export function headers() {
+  const token = load('token');
+
+  return {
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+}
+
+export async function authFetch(url, option) {
+  return fetch(url, {
+    ...option,
+    headers: headers(),
+  });
+}
+ */
