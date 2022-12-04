73ba510a50edd35feb7b9c65c62080a520d50c04
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-04 23:20:33 +0100

    Updated with apiBase with new API links

--- src/js/api/auth/apiBase.js
+++ src/js/api/auth/apiBase.js
@@ -4,3 +4,6 @@ export const API_HOST = 'https://api.noroff.dev/';
 export const API_REGISTER = 'api/v1/auction/auth/register';
 // API login link
 export const API_LOGIN = 'api/v1/auction/auth/login';
+// API Complete login link
+export const API_HOST_LOGIN =
+  'https://api.noroff.dev/api/v1/auction/auth/login';
