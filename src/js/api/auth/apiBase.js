da655432b91c6adf8d7a99363f79fb0e65c4ae72
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2022-12-04 07:10:17 +0100

    Creating of API Host and fetch files

---
+++ src/js/api/auth/apiBase.js
@@ -0,0 +1,6 @@
+// API base URL
+export const API_HOST = 'https://api.noroff.dev/';
+// API register link
+export const API_REGISTER = 'api/v1/auction/auth/register';
+// API login link
+export const API_LOGIN = 'api/v1/auction/auth/login';
