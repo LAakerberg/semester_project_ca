b139c1c4bdbb571a9a05469ec25527accb871351
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-04 23:21:56 +0100

    Moved .onclick functions from this file into login.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,9 @@ console.log('Intentional Syntax Error...');
 const signUpModal = document.querySelector('#signupModal');
 const signUpBtn = document.querySelector('#registerBtn');
 const signUpBtnClose = document.querySelector('#signup-close');
-const modal = document.querySelector('#loginModal');
+/* const modal = document.querySelector('#loginModal');
 const btn = document.querySelector('#loginBtn');
-const btnClose = document.querySelector('#close');
+const btnClose = document.querySelector('#close'); */
 
 signUpBtn.onclick = function () {
   signUpModal.style.display = 'block';
@@ -15,16 +15,17 @@ signUpBtnClose.onclick = function () {
   signUpModal.style.display = 'none';
 };
 
-btn.onclick = function () {
+/* btn.onclick = function () {
   modal.style.display = 'block';
 };
 
 btnClose.onclick = function () {
   modal.style.display = 'none';
-};
+}; */
 
-window.onclick = function (event) {
+/* window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
   }
 };
+ */
