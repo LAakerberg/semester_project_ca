66f5d4624ee3cc359266eb07dee8780e79ece36f
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-04 23:21:11 +0100

    Added new index.js to handle export/import files for login

---
+++ src/js/api/index.js
@@ -0,0 +1,9 @@
+// Login in user if the value is correct
+import { loginUser } from './login/login.js';
+
+loginUser();
+
+// Open the login form/modal
+import { openLoginForm } from './login/login.js';
+
+openLoginForm();
