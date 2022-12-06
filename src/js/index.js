8eed84c0af6259248743ca668adf1096e6c6db44
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-06 23:29:53 +0100

    Moved imported JS functions to new index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log('Intentional Syntax Error...');
+// Login in user if the value is correct
+import { loginUser } from './api/login/login.js';
+
+loginUser();
+
+// Open the login form/modal
+import { openLoginForm } from './api/login/login.js';
+
+openLoginForm();
+
+import { openRegisterForm } from './api/login/register.js';
+
+openRegisterForm();
+
+import { registerUser } from './api/login/register.js';
+
+registerUser();
