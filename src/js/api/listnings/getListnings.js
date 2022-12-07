b451e40c946c1ee3bcabc99c2077e4a4aee15c1b
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-07 23:01:37 +0100

    Added an for loop of listings

--- src/js/api/listnings/getListnings.js
+++ src/js/api/listnings/getListnings.js
@@ -21,35 +21,47 @@ export async function getListings(url) {
 
     const json = await response.json(API_HOST_LISTINGS);
     const listingsResults = json;
-    const listingsID = listingsResults[0].id;
-    const listingsTitle = listingsResults[0].title;
-    const listingsCreated = listingsResults[0].created;
 
     console.log(response);
     console.log(listingsResults);
-    console.log('Hello?');
 
-    if (response.ok === true) {
+    const dateRequested = new Date(`${listingsResults[0].created}`);
+    // Formats the date from the request to be more user friendly and readable
+    const dateFormatted = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    };
+    const newFormat = dateRequested.toLocaleDateString('en-GB', dateFormatted);
+
+    /*     if (response.ok === true) {
       console.log('True');
-    }
+    } */
 
-    auctionListings.innerHTML = `
-    <!-- Product card -->
-    <div
-      class="bg-slate-600 border border-slate-500 rounded-lg w-44 h-48"
-    >
-      <div class="h-4/6 border-b-2 border-slate-500">
-        <img
-          src="/assets/img/head-img.jpg"
-          class="img-style rounded-t-lg p-1"
-        />
-      </div>
-      <div class="p-1">${listingsID}</div>
-      <div class="p-1">${listingsTitle}</div>
-      <div class="p-1">${listingsCreated}</div>
-    </div>
-    <!-- Product card END -->
-    `;
+    for (let i = 0; i < listingsResults.length; i++) {
+      if (response.ok === true) {
+        const listingsID = listingsResults[i].id;
+        const listingsTitle = listingsResults[i].title.slice(0, 20);
+        const listingsCreated = listingsResults[i].created;
+
+        auctionListings.innerHTML += `
+        <!-- Product card -->
+        <div
+          class="bg-slate-600 outline outline-1 hover:outline-2 outline-slate-500 rounded-lg w-44 h-48 shadow-lg hover:shadow-slate-400/50"
+        >
+          <div class="h-4/6 border-b-2 border-slate-500">
+            <img
+              src="/assets/img/head-img.jpg"
+              class="img-style rounded-t-lg p-1"
+            />
+          </div>
+          <div class="p-1">${listingsTitle}</div>
+          <div class="p-1">${newFormat}</div>
+        </div>
+        <!-- Product card END -->
+        `;
+      }
+    }
   } catch (error) {
     console.log('Error loading the auction house listings');
     console.log('Hello?');
