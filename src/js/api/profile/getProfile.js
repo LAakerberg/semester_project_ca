1e49d3dfaad76d1daf99545cf4fb8d0f99e15381
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-12 14:13:27 +0100

    Added getProfile.js with profile info + listings

---
+++ src/js/api/profile/getProfile.js
@@ -0,0 +1,108 @@
+const profileInsert = document.querySelector('#profile');
+const activeListings = document.querySelector('#active-list');
+
+// Importing API link + fetch functions for async function
+import { API_HOST_PROFILES } from '../auth/apiBase.js';
+import { authFetch } from '../auth/authFetch.js';
+import { headers } from '../auth/authFetch.js';
+
+const method = 'GET';
+
+profileInsert.innerHTML = ``;
+activeListings.innerHTML = ``;
+
+const profileIdentify = localStorage.getItem('myName');
+const profileEmail = localStorage.getItem('myEmail');
+
+export async function getProfile(url) {
+  try {
+    const response = await authFetch(
+      url,
+      {
+        method,
+      },
+      headers()
+    );
+
+    const json = await response.json(API_HOST_PROFILES);
+    const profileInfo = json;
+
+    console.log(profileInfo);
+
+    if (response.ok === true && profileEmail === profileInfo.email) {
+      const profileName = profileInfo.name;
+      const profileEmail = profileInfo.email;
+      const profileCredit = profileInfo.credits;
+      const profileAvatar = profileInfo.avatar;
+      const profileWins = profileInfo.wins.length;
+      const profileCount = profileInfo._count.listings;
+
+      profileInsert.innerHTML += `
+        <!-- Product card -->
+        <div class="flex-shrink w-64">
+        <div
+          class="bg-slate-700 outline outline-1 outline-slate-500 rounded-md h-48"
+        >
+          <img src="${profileAvatar}" id="profileImg" class="rounded-md h-48" />
+        </div>
+      </div>
+      <div class="flex-1"><span>Username:</span>
+      ${profileName}
+        <div>
+          <ul>
+            <li><span>Email:</span> ${profileEmail}</li>
+            <li><span>Credit:</span> ${profileCredit}</li>
+            <li><span>Wins:</span> ${profileWins}</li>
+            <li><span>Count:</span> ${profileCount}</li>
+          </ul>
+        </div>
+      </div>
+      <div class="flex-grow-0">
+      <ul>
+        <li>Add new listings</li>
+        <li>Edit profile</li>
+      </ul>
+      </div>
+        <!-- Product card END -->
+        `;
+
+      for (let i = 0; i < profileInfo.listings.length; i++) {
+        const dateRequested = new Date(`${profileInfo.listings[0].created}`);
+        // Formats the date from the request to be more user friendly and readable
+        const dateFormatted = {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+        };
+        const newFormat = dateRequested.toLocaleDateString(
+          'en-GB',
+          dateFormatted
+        );
+
+        activeListings.innerHTML += `
+        <!-- Product card -->
+        <div
+          class="bg-slate-600 outline outline-1 hover:outline-2 outline-slate-500 rounded-lg w-44 h-48 shadow-lg hover:shadow-slate-400/50"
+        >
+          <div class="h-4/6 border-b-2 border-slate-500">
+            <img
+              src="${profileInfo.listings[i].media[0]}"
+              class="img-style rounded-t-lg p-1 w-44 h-32"
+            />
+          </div>
+          <div class="p-1">${profileInfo.listings[i].title}</div>
+          <div class="p-1">${newFormat}</div>
+        </div>
+        <!-- Product card END -->
+        `;
+      }
+    } else {
+      console.log('No match');
+    }
+  } catch (error) {
+    console.log('Error loading the auction house listings');
+    console.log('Hello?');
+  }
+}
+
+getProfile(`${API_HOST_PROFILES}${profileIdentify}?_listings=true`);
