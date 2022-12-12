c990d5f5eb0ea8d83ad5f13c734232eae7518fad
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-12 14:11:43 +0100

    Added new API link and cleaned up old once

--- src/js/api/auth/apiBase.js
+++ src/js/api/auth/apiBase.js
@@ -1,11 +1,13 @@
 // API base URL
-export const API_HOST = 'https://api.noroff.dev/';
+const API_HOST = 'https://api.noroff.dev/';
 // API register link
-export const API_REGISTER = 'api/v1/auction/auth/register';
+const API_REGISTER = 'api/v1/auction/auth/register';
 // API login link
-export const API_LOGIN = 'api/v1/auction/auth/login';
+const API_LOGIN = 'api/v1/auction/auth/login';
 // API Get listings / products
-export const API_LISTINGS = 'api/v1/auction/listings/';
+const API_LISTINGS = 'api/v1/auction/listings/';
+// API Get profiles
+const API_PROFILES = 'api/v1/auction/profiles/';
 // API Complete login link
 export const API_HOST_LOGIN = `${API_HOST}${API_LOGIN}`;
 // API Complete register link
@@ -13,3 +15,9 @@ export const API_HOST_REGISTER = `${API_HOST}${API_REGISTER}`;
 
 // API Complete listings / products
 export const API_HOST_LISTINGS = `${API_HOST}${API_LISTINGS}`;
+
+// API Complete profile
+export const API_HOST_PROFILES = `${API_HOST}${API_PROFILES}`;
+
+//https://api.noroff.dev/api/v1/auction/profiles/
+//https://api.noroff.dev/api/v1/auction/profiles/
