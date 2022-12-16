c134e6218242a9f191e1ef6b5416e750db9cb296
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-16 18:25:50 +0100

    Added new breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,28 @@
 module.exports = {
   content: ['index.html', './pages/**/*.html', './src/**/*.{html,js}'],
   theme: {
+    screens: {
+      mobile: '410px',
+      // => @media (min-width: 410px) { ... }
+
+      tablet: '550px',
+      // => @media (min-width: 550px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
