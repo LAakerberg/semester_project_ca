576b3d5037e31b0bb810f500ce717273c9298775
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-12-18 02:40:48 +0100

    Clean code that's unused

--- src/js/api/profile/index.js
+++ src/js/api/profile/index.js
@@ -1,10 +1,6 @@
 import { requestProfile } from './getProfile';
 requestProfile();
 
-/* import { collectProfileData } from './edit/editProfile.js';
-
-collectProfileData(); */
-
 import { logOutUser } from '../../components/function.js';
 
 logOutUser();
